fc82467630f4f03dd96a593ce1d5fcbc67ee8387
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-26 14:36:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your code here
+const evaluatedText = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+
+const updateLetterCount = () => {
+  const text = evaluatedText.value;
+  const count = text.length;
+  letterCount.textContent = count;
+};
+
+
+evaluatedText.addEventListener("input", updateLetterCount);
+
+
+updateLetterCount();
